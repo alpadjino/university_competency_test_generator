a99152f756ab2a38ba0e51af7ca468717984a169
Косаревский Денис <denis.kosarevskiy@restream.ru>
2026-06-08 15:05:58 +0300

    fix: docker-compose network

--- sql/SCHEMAS.sql
+++ sql/SCHEMAS.sql
@@ -10,16 +10,28 @@ CREATE TYPE question_category AS ENUM ('A', 'B', 'C');
 CREATE TYPE question_type AS ENUM ('Closed', 'Open');
 CREATE TYPE question_subtype AS ENUM ('One', 'Multiple', 'Matching', 'CorrectSequence', 'Addition', 'DetailedAnswer');
 
+CREATE TYPE test_status AS ENUM ('in_progress', 'done');
+
+CREATE TABLE tests (
+    id SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    status test_status DEFAULT 'in_progress',
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    files TEXT[]
+);
+
 CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
-    test_id INTEGER REFERENCES tests(id),
+    test_id INTEGER,
     "text" TEXT NOT NULL,
     "question" TEXT NOT NULL,
     category question_category NOT NULL,
     "type" question_type NOT NULL,
     subtype question_subtype NOT NULL,
     standard_answer TEXT,
-    order INTEGER NOT NULL,
+    "order" INTEGER NOT NULL,
     options JSONB,
 
     CONSTRAINT fk_test
@@ -33,18 +45,6 @@ CREATE TABLE question_options (
     question_id INTEGER REFERENCES questions(id),
     "text" TEXT NOT NULL,
     is_true BOOLEAN NOT NULL DEFAULT false
-);
-
-CREATE TYPE test_status AS ENUM ('in_progress', 'done');
-
-CREATE TABLE tests (
-    id SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    status test_status DEFAULT 'in_progress',
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW(),
-    files TEXT[]
 );
 
 CREATE TABLE test_competencies (
